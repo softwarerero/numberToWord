8d92c874e4f53b94a83d70efa03402c7ab828fbb
softwarerero <el@softwarerero.com>
2019-10-07 18:23:59 -0300

    Add numberToOrdinal (Spanish)

---
+++ numberToOrdinal.es.js
@@ -0,0 +1,18 @@
+const esNumberWords = [
+	['', 'primera', 'segunda', 'tercera', 'cuarta', 'quinta', 'sexta', 'septima', 'octava', 'novena'],
+	['', 'decima', 'vigésima', 'trigésima', 'cuadragésima', 'quincuagésima', 'sexagésimo',
+		'septuagésima', 'octogésima', 'nonagésima'],
+	['', 'centésima'],
+]
+
+
+export default number => {
+    if (number > 199) throw new Error('The number is too big, we have no words for this')
+    return number
+        .toString()
+        .split('').
+        reverse()
+        .map((n, i) => esNumberWords[i][n] || esNumberWords[i][0])
+        .reverse()
+        .join(' ')
+}
---
+++ test/numberToOrdinal.es.test.js
@@ -0,0 +1,17 @@
+import assert from 'assert'
+import numberToOrdinal from '../numberToOrdinal.es.js'
+
+const equal = (no, text) => () => assert.equal(numberToOrdinal(no), text)
+
+describe.only('es units', () => {
+    it('1', equal(1, 'primera'))
+    it('5', equal(5, 'quinta'))
+    it('9', equal(9, 'novena'))
+    it('11', equal(11, 'decima primera'))
+    it('111', equal(111, 'centésima decima primera'))
+    it('123', equal(123, 'centésima vigésima tercera'))
+    it('987', () => { 
+        const fn = () => numberToOrdinal(987)
+        assert.throws(fn, Error, 'The number is too big, we have no words for this')
+    })
+})
